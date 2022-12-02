3c2d75801858f9560ef0d42e02893996e0465873
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2022-12-02 12:03:46 +0700

    Create Latihan1.java

---
+++ src/nella211022/Latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nella211022;
+
+/**
+ *
+ * @author Asus
+ */
+public class Latihan1 {
+    public static void main(String[] args) {
+       int grade = 68; 
+        if( grade > 60 ){ 
+         System.out.println("Congratulations!"); 
+         System.out.println("You passed!"); 
+        } 
+        else{ 
+         System.out.println("Sorry you failed"); 
+        }  
+    }
+    
+}
