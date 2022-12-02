3b5150a9846bc410809159b7a7b2805d4ba9c1f1
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2022-12-02 12:04:45 +0700

    Create ganjilgenap.java

---
+++ src/teori131022/ganjilgenap.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teori131022;
+
+/**
+ *
+ * @author Asus
+ */
+public class ganjilgenap {
+    int a = 10;
+    int b= 11;
+    int hasil;
+    
+    hasil = a+b;
+}
