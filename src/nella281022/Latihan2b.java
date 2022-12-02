fa5f9a1ea0437a140a60e4ef9b485518776a2833
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2022-12-02 12:04:21 +0700

    Create Latihan2b.java

---
+++ src/nella281022/Latihan2b.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nella281022;
+
+/**
+ *
+ * @author Asus
+ */
+public class Latihan2b {
+        public static void main(String[] args) {
+        int i = 0;
+        String days[] = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        
+        while (i < 7){
+            System.err.println(days[i]);
+            i++;
+        }
+    }
+}
