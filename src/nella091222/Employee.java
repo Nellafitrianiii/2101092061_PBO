128114205734be255d839a69c2e5cf7237ec3b58
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2022-12-09 11:52:31 +0700

    Create Employee.java

---
+++ src/nella091222/Employee.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nella091222;
+
+/**
+ *
+ * @author Asus
+ */
+public class Employee extends Person{
+    public String getName(){ 
+    System.out.println("Student: getName"); 
+    return name; 
+    } 
+
+}
