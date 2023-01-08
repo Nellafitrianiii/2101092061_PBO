ae24372593d9d939cf6236540ecf765b8563e143
Nellafitriani09 <114406491+Nellafitrianiii@users.noreply.github.com>
2023-01-08 23:30:05 +0700

    Create AnggotaDaoImpl.java

---
+++ nellaa/model/AnggotaDaoImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nellaa.model;
+
+import java.util.List;
+import nella.model.Anggota;
+
+/**
+ *
+ * @author Asus
+ */
+public class AnggotaDaoImpl implements AnggotaDao {
+
+    @Override
+    public Anggota save(Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Anggota Update(int index, Anggota anggota) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void delete(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Anggota getAnggota(int index) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Anggota> getAllAnggota() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
